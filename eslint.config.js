41834c78372660a45e9e292d87ed4a5a3401ac0e
hjpark625 <zero950@gmail.com>
2025-03-02 18:32:47 +0900

    feat: eslint, prettier config 설정 및 적용

---
+++ eslint.config.js
@@ -0,0 +1,44 @@
+// @ts-check
+const eslint = require('@eslint/js')
+const tseslint = require('typescript-eslint')
+const angular = require('angular-eslint')
+const globals = require('globals')
+const prettierConfig = require('eslint-config-prettier')
+const prettierPlugin = require('eslint-plugin-prettier')
+
+module.exports = tseslint.config(
+  {
+    files: ['**/*.ts'],
+    extends: [
+      eslint.configs.recommended,
+      ...tseslint.configs.recommended,
+      ...tseslint.configs.stylistic,
+      ...angular.configs.tsRecommended
+    ],
+    plugins: {
+      '@typescript-eslint': tseslint.plugin,
+      prettier: prettierPlugin
+    },
+    languageOptions: {
+      parser: tseslint.parser,
+      ecmaVersion: 2022,
+      globals: {
+        ...globals.es2022,
+        ...globals.node,
+        ...globals.browser
+      }
+    },
+    processor: angular.processInlineTemplates,
+    rules: {
+      ...prettierConfig.rules,
+      'prettier/prettier': 'warn',
+
+      '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_', varsIgnorePattern: '^_' }]
+    }
+  },
+  {
+    files: ['**/*.html'],
+    extends: [...angular.configs.templateRecommended, ...angular.configs.templateAccessibility],
+    rules: {}
+  }
+)
---
+++ prettier.config.js
@@ -0,0 +1,9 @@
+/** @type {import('prettier').Config} */
+module.exports = {
+  singleQuote: true,
+  trailingComma: 'none',
+  tabWidth: 2,
+  useTabs: false,
+  semi: false,
+  printWidth: 120
+}
