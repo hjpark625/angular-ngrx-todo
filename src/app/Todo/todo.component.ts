37f1f94087d0f92917a5b97bb64c6479e1b157d0
hjpark625 <zero950@gmail.com>
2025-03-02 19:33:05 +0900

    feat: todo component 초안

--- src/app/Todo/todo.component.ts
+++ src/app/Todo/todo.component.ts
@@ -1,6 +1,47 @@
-import { Component } from '@angular/core'
+import { Component, inject } from '@angular/core'
+import { CommonModule } from '@angular/common'
+import { Store } from '@ngrx/store'
+
+import { selectInputValue, selectTodos } from '@/stores/selectors/todo.selector'
+
+import type { OnInit } from '@angular/core'
+import { addInputValue, addTodo, deleteTodo, toggleTodo } from '@/stores/actions/todo.action'
 
 @Component({
-  templateUrl: './todo.component.html'
+  templateUrl: './todo.component.html',
+  imports: [CommonModule]
 })
-export class TodoComponent {}
+export class TodoComponent implements OnInit {
+  store = inject(Store)
+
+  inputVal$ = this.store.select(selectInputValue)
+  todos$ = this.store.select(selectTodos)
+
+  onAddInputValue(event: Event) {
+    const target = event.target
+    if (target instanceof HTMLInputElement) {
+      const value = target.value
+      this.store.dispatch(addInputValue({ payload: value }))
+      return
+    } else return
+  }
+
+  onAddTodo(event: Event) {
+    event.preventDefault()
+    this.store.dispatch(addTodo())
+    return
+  }
+
+  onDeleteTodo(todoId: number) {
+    this.store.dispatch(deleteTodo({ payload: todoId }))
+  }
+
+  onToggleTodo(todoId: number) {
+    this.store.dispatch(toggleTodo({ payload: todoId }))
+  }
+
+  ngOnInit(): void {
+    this.todos$.subscribe((val) => console.log(val))
+    this.inputVal$.subscribe((val) => console.log(val))
+  }
+}
