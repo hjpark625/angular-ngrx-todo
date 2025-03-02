f3aa78869ce78477a8f22f795803ecd7f58ba3ac
hjpark625 <zero950@gmail.com>
2025-03-02 18:51:28 +0900

    feat: todo page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,6 @@
 import { Routes } from '@angular/router'
 
 export const routes: Routes = [
-  { path: '', loadComponent: () => import('@app/Home/home.component').then((C) => C.HomeComponent) }
+  { path: '', loadComponent: () => import('@app/Home/home.component').then((C) => C.HomeComponent) },
+  { path: 'todo', loadComponent: () => import('@app/Todo/todo.component').then((C) => C.TodoComponent) }
 ]
