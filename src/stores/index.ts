c6eac4311e9d865048e4a8af37be710999fa18ff
hjpark625 <zero950@gmail.com>
2025-04-19 23:40:34 +0900

    feat: rootReducer 내 스토어 타입 추가

--- src/stores/index.ts
+++ src/stores/index.ts
@@ -5,3 +5,5 @@ import { todoReducer } from '@/stores/reducers/todo.reducer'
 export const rootReducer = combineReducers({
   todos: todoReducer
 })
+
+export type RootState = ReturnType<typeof rootReducer>
